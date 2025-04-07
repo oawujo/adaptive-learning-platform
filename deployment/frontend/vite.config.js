42a42d06b593846f66ffc7ab5461ecb53f3ed68d
Kech <77866313+oawujo@users.noreply.github.com>
2025-04-07 21:36:56 +0100

    correcting vite.config.js file

--- deployment/frontend/vite.config.js
+++ deployment/frontend/vite.config.js
@@ -2,6 +2,11 @@
 export default defineConfig({
   root: ".",
   build: {
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
+
+  export default defineConfig({
+    plugins: [react()],
     outDir: "dist",
   },
 });
